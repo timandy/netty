690a53066d762017c048727bafb3248f1529a4d4
TimAndy <xuchonglei@126.com>
2020-05-15 14:28:53 +0800

    Reduce duplicated check for charset

--- buffer/src/main/java/io/netty/buffer/Unpooled.java
+++ buffer/src/main/java/io/netty/buffer/Unpooled.java
@@ -659,9 +659,8 @@ public final class Unpooled {
             CharBuffer buf = (CharBuffer) string;
             if (buf.hasArray()) {
                 return copiedBuffer(
-                        buf.array(),
-                        buf.arrayOffset() + buf.position() + offset,
-                        length, charset);
+                        CharBuffer.wrap(buf.array(), buf.arrayOffset() + buf.position() + offset, length),
+                        charset);
             }
 
             buf = buf.slice();
